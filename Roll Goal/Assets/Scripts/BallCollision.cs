e425ae11097795f5a467747dd0d2e3830a885e82
Chris is Awesome <chrisstarkofficial@gmail.com>
2020-07-05 13:45:40 -0700

    Added BallCollision script

---
+++ Roll Goal/Assets/Scripts/BallCollision.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+	[Header("Refs")]
+	[SerializeField] PhysicsMaterial2D wallMat;
+	[SerializeField] CircleCollider2D ballCollider;
+	[Header("Data")]
+	[SerializeField] float bounceStartValue;
+	[SerializeField] [Range(0, 0.1f)] float bounceDecrement;
+	[SerializeField] [Range(0, 0.001f)] float frictionIncrement;
+
+	void Awake()
+	{
+		// Default material
+		wallMat.bounciness = bounceStartValue;
+		wallMat.friction = 0f;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// If collision with flag, mark level as complete
+		if (other.gameObject.tag == "Flag")
+		{
+			// TODO: Level complete!
+
+			Debug.Log("Completed level!");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		// Decrement bounciness and increment friction with every bounce
+		wallMat.bounciness -= bounceDecrement;
+		wallMat.friction += frictionIncrement;
+
+		// Reset collider to make bouncing change take effect (Workaround for Unity bug)
+		if (transform.localScale.x == 1)
+		{
+			ballCollider.isTrigger = true;
+			ballCollider.isTrigger = false;
+		}
+	}
+}
--- Roll Goal/Assets/Scripts/LevelData.cs
+++ Roll Goal/Assets/Scripts/LevelData.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelData : MonoBehaviour
@@ -29,6 +28,6 @@ public class LevelData : MonoBehaviour
 	public void GrantBall()
 	{
 		// Give player a new ball
-		//Instantiate(ballPrefab, startPos, Quaternion.identity).transform.parent = ballParent;
+		Instantiate(ballPrefab, startPos, Quaternion.identity).transform.parent = ballParent;
 	}
 }
